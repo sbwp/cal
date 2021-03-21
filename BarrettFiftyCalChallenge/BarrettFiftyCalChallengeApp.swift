b9005d2ca666e2d09f345a820041f0d5ebac2a35
Sabrina Bea <me@sabrinabea.com>
2021-03-21 00:49:54 -0400

    Initial Commit

---
+++ BarrettFiftyCalChallenge/BarrettFiftyCalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaFiftyCalChallengeApp.swift
+//  BeaFiftyCalChallenge
+//
+//  Created by Sabrina Bea on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BeaFiftyCalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
